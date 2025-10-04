1aa6f16a2a08f4ebc04ed9a2f68dd59483405709
Umer Iqbal <official.umer.iqbal@gmail.com>
2025-10-05 04:32:00 +0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const https = require('https')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index1.html")
+})
+
+app.post("/", (req, res) => {
+    let category = req.body.category;
+
+    let url = "https://v2.jokeapi.dev/joke/" + category;
+
+    https.get(url, (response) => {
+        let rawData = "";
+    
+        // Collect data chunks
+        response.on("data", (chunk) => {
+            rawData += chunk;
+        });
+    
+        response.on("end", () => {
+            const jokeData = JSON.parse(rawData);
+            if (jokeData.type === "single") {
+                let joke = jokeData.joke;
+                res.render("singleJoke", { joke: joke })
+            } else {
+                let setup = jokeData.setup;
+                let delivery = jokeData.delivery;
+                res.render("twoPartjoke", { setup: setup, delivery: delivery })
+            }
+        })
+    })
+})
+
+app.post("/again", (req, res)=>{
+    res.redirect("/")
+})
+
+app.listen(port, () => {
+    console.log(`https://localhost:${port}`)
+})
