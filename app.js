64a9626502bfc6493386e39abc951967b584e104
Umer Iqbal <142382304+m-umer-iqbal@users.noreply.github.com>
2025-10-05 04:58:10 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index1.html")
+    res.sendFile(__dirname + "/index.html")
 })
 
 app.post("/", (req, res) => {
